c98f03b9f0034364a4eb3bf6330d4dc7613dd50e
Jaypee <ds.jaypee@gmail.com>
2017-04-15 21:55:08 +0200

    Structured the processing of a track as a separate function Was led to this change because of Klaus Schulze "Mirage" where a track is a whole side composed of sub-tracks

--- discogs.py
+++ discogs.py
@@ -9,31 +9,30 @@ if len(sys.argv) < 2:
         sys.stderr.write('Usage: {} <release ID>\n'.format(sys.argv[0]))
         sys.exit(1)
 
-release_id = int(sys.argv[1])
-
-opener = urllib2.build_opener()
-opener.addheaders = [('User-Agent', 'AudacityTaggerApp/3.0'), ('Content-Encoding', 'gzip')]
-json_response = opener.open('https://api.discogs.com/releases/{}'.format(release_id)) 
-
-release = json.load(json_response)
-
-for artist in release['artists']:
-	aname = artist['name']
-
-album = release['title']
-year= release['year']
-for genre in release['genres']:
-	gname = genre
-
 last_face = ''
 end_ts = 0.0
 labels = []
 offset = 0
-for track in release['tracklist']:
-    if track['type_'] != 'track':
-        continue
+
+def track_name_to_number(tname):
+    if tname.isnumeric():
+        return int(tname)
     else:
-	tposition = [track['position'][0], track['position'][1:]]	
+        if '.' in tname:
+            label = list(tname.split('.')[1])
+        else:
+            label = list(tname)
+        return ord(label[0]) - ord('a') + 1
+
+
+def process_track(track):
+
+	global last_face
+        global end_ts
+        global labels
+        global offset
+
+        tposition = [track['position'][0], track['position'][1:]]	
 	ttitle = track['title']
 	tduration = track['duration']
 	tface = list(tposition)[0]
@@ -71,7 +70,7 @@ for track in release['tracklist']:
     	# Such records as Keith Jarrett's Koeln Concert have one track per face: A, B, C, D
     	# So A is equivalent to A1
     	try:	
-        	track_number = offset + int(tposition[1])
+        	track_number = offset + track_name_to_number(tposition[1]) 
     	except:
         	track_number = offset + 1
         
@@ -89,10 +88,38 @@ for track in release['tracklist']:
 	track_io.write( '<!-- Generated from discogs release ID {} -->\n'.format(release_id) )
 	track_io.close()
 
-labels_file_name = '{}-{}-{}.txt'.format(aname,album,last_face)
-print 'Last face: labels for face {} in {}'.format(last_face, labels_file_name)
-labels_io = open(labels_file_name, 'w')
-for label in labels:
-	labels_io.write('{}\n'.format(label))
 
-labels_io.close()
+if __name__ == '__main__':
+    release_id = int(sys.argv[1])
+
+    opener = urllib2.build_opener()
+    opener.addheaders = [('User-Agent', 'AudacityTaggerApp/3.0'), ('Content-Encoding', 'gzip')]
+    json_response = opener.open('https://api.discogs.com/releases/{}'.format(release_id)) 
+
+    release = json.load(json_response)
+
+    for artist in release['artists']:
+            aname = artist['name']
+
+    album = release['title']
+    year= release['year']
+    for genre in release['genres']:
+            gname = genre
+
+    for track in release['tracklist']:
+        track_type = track['type_']
+        if track_type == 'track':
+            process_track(track)
+        elif track_type == 'index':
+            sub_tracks = track['sub_tracks']
+            for sub_track in sub_tracks:
+                process_track(sub_track)
+
+    labels_file_name = '{}-{}-{}.txt'.format(aname,album,last_face)
+    print 'Last face: labels for face {} in {}'.format(last_face, labels_file_name)
+    labels_io = open(labels_file_name, 'w')
+    for label in labels:
+            labels_io.write('{}\n'.format(label))
+
+    labels_io.close()
+
